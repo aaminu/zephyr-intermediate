39f9e69b7942453719114b08c2fa71ba567bbe95
Abdulrahman Aminu <aminuabdulrahmandayo@gmail.com>
2026-08-23 12:26:42 -0500

    Synchronization
    Signed-off-by: Abdulrahman Aminu <aminuabdulrahmandayo@gmail.com>

--- app/src/main.c
+++ app/src/main.c
@@ -9,6 +9,11 @@ LOG_MODULE_REGISTER(demo, LOG_LEVEL_DBG);
 #define PRIO_MED 5
 #define PRIO_HGH 3
 #define PRIO_COOP -1
+#define INCREMENTS 1000000 /* each thread increments this many times */
+
+static K_MUTEX_DEFINE(counter_mutex);
+
+/* Shared state - intentionally unprotected */ static volatile uint32_t counter;
 
 void t_low_fn(void *p1, void *p2, void *p3)
 {
@@ -18,12 +23,19 @@ void t_low_fn(void *p1, void *p2, void *p3)
     }
 }
 
-void t_med_fn(void *p1, void *p2, void *p3)
-{
-    while (1) {
-        LOG_DBG("T_LOW running");
-        k_msleep(200);
-    }
+void t_med_fn(void *p1, void *p2, void *p3) 
+{ 
+   
+    const char *name = k_thread_name_get(k_current_get()); 
+    LOG_INF("[%s] Started: %u", name, counter); 
+    
+    for (int i = 0; i < INCREMENTS; i++) 
+    { 
+        k_mutex_lock(&counter_mutex, K_FOREVER);
+        counter++; 
+        k_mutex_unlock(&counter_mutex);
+    } 
+    LOG_INF("[%s] finished: %u", name, counter); 
 }
 
 void t_high_fn(void *p1, void *p2, void *p3)
@@ -36,33 +48,27 @@ void t_high_fn(void *p1, void *p2, void *p3)
 
 void coop_fn(void *p1, void *p2, void *p3)
 {
-    while (1) {
+    for (int i = 0; i < 5; i++) 
+    {
+        k_busy_wait(40000);
 
-        for (int i = 0; i < 5; i++) {
-            k_busy_wait(40000);
-
-            LOG_INF("T_COOP step %d/5 - still holding CPU tick=%u",
-                    i + 1, k_uptime_get_32());
-        }
-
-        LOG_INF("T_COOP yielding");
-
-        k_yield();
+        LOG_INF("T_COOP step %d/5 - still holding CPU tick=%u",
+                i + 1, k_uptime_get_32());
     }
+
+    LOG_INF("T_COOP yielding");
+    k_yield();
 }
 
-K_THREAD_DEFINE(thread_a, STACK_SIZE, t_low_fn,
-                NULL, NULL, NULL, PRIO_LOW, 0, 0);
-K_THREAD_DEFINE(thread_b, STACK_SIZE, t_med_fn,
-                NULL, NULL, NULL, PRIO_MED, 0, 0);
-K_THREAD_DEFINE(thread_c, STACK_SIZE, t_high_fn,
-                NULL, NULL, NULL, PRIO_HGH, 0, 0);
-K_THREAD_DEFINE(thread_d, STACK_SIZE, coop_fn,
-                NULL, NULL, NULL, PRIO_COOP, 0, 0);
+K_THREAD_DEFINE(thread_a, STACK_SIZE, t_low_fn, NULL, NULL, NULL, PRIO_LOW, 0, 0); 
+K_THREAD_DEFINE(thread_b, STACK_SIZE, t_med_fn, NULL, NULL, NULL, PRIO_MED, 0, 0); 
+K_THREAD_DEFINE(thread_c, STACK_SIZE, t_med_fn, NULL, NULL, NULL, PRIO_MED, 0, 0); 
+// K_THREAD_DEFINE(thread_d, STACK_SIZE, coop_fn, NULL, NULL, NULL, PRIO_COOP, 0, 0);
 
 
 int main(void)
 {
+    printk("Hello World");
     return 0;
 }
 
